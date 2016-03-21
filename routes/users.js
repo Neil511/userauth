1d8fb518b3db6c3cbb85dc965dc1b8dc8e667566
Neil Lohana <Neil_lohana@hotmail.com>
2016-03-20 21:16:04 -0400

    Fixed views, added all view layouts

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+
+router.get('/register', function(req, res, next){
+    res.render('register', {
+        'title': 'Register'
+    });
+});
+
+router.get('/signin', function(req, res, next){
+    res.render('signin', {
+        'title': 'Sign In'
+    });
 });
 
 module.exports = router;
